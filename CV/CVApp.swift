3b6d714bd4caea46ab53de647becb36a1ce258f2
Sebastian Corradi <SebastianCorradi@gmail.com>
2025-03-20 18:29:52 -0300

    Initial Commit

---
+++ CV/CVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVApp.swift
+//  CV
+//
+//  Created by Sebastian Eugenio Perez Corradi on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
